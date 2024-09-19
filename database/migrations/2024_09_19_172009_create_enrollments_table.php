d33de6fad75114e30e5caaecae73224c6e080b11
zying11 <yingying22226@gmail.com>
2024-09-20 01:28:08 +0800

    add enrollment table

---
+++ database/migrations/2024_09_19_172009_create_enrollments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id'); 
+            $table->unsignedBigInteger('subject_id');
+            $table->unsignedBigInteger('study_level_id');
+            $table->unsignedBigInteger('lesson_id');
+            $table->timestamps();
+    
+            // Foreign key constraints
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+            $table->foreign('study_level_id')->references('id')->on('study_level')->onDelete('cascade');
+            $table->foreign('lesson_id')->references('id')->on('lessons')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
