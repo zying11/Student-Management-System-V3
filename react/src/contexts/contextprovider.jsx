b21902fe9a4d1173ac4e14dcda33786fbb787357
zying11 <yingying22226@gmail.com>
2024-10-19 00:34:21 +0800

    fix the unauthorized issue when refreshed the page or directly access url

--- react/src/contexts/contextprovider.jsx
+++ react/src/contexts/contextprovider.jsx
@@ -68,7 +68,9 @@ const StateContext = createContext({
 });
 
 export const ContextProvider = ({ children }) => {
-  const [user, setUser] = useState({});
+  const [user, setUserState] = useState(
+    JSON.parse(localStorage.getItem("USER")) || {} 
+  );
   const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
   const [csrfToken, setCsrfToken] = useState(null);
 
@@ -78,6 +80,15 @@ export const ContextProvider = ({ children }) => {
       localStorage.setItem("ACCESS_TOKEN", token);
     } else {
       localStorage.removeItem("ACCESS_TOKEN");
+    }
+  };
+
+  const setUser = (user) => {
+    setUserState(user);
+    if (user) {
+      localStorage.setItem("USER", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("USER");
     }
   };
 
